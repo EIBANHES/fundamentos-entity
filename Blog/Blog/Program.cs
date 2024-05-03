1f968adcbed179095e0a4a3cf5c4ebb0bab6e429
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-05-03 19:34:57 -0400

    Criando migracao no banco de dados com EF Core

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -6,29 +6,5 @@ class Program
     static void Main(string[] args)
     {
         using var context = new BlogDataContext();
-        //context.Users.Add(new User
-        //{
-        //    Name = "André Baltieri",
-        //    Slug = "andrebaltieri",
-        //    Email = "andre@balta.io",
-        //    Bio = "9x Microsoft MVP",
-        //    Image = "https://balta.io",
-        //    PasswordHash = "123098457"
-        //});
-        //context.SaveChanges();
-
-        var user = context.Users.FirstOrDefault();
-        var post = new Post {
-            Author = user,
-            Body = "Teste",
-            Category = new Category { Name = "Backend", Slug = "backend" },
-            CreateDate = DateTime.Now,
-            //LastUpdateDate = DateTime.Now,
-            Slug = "meu-artigo",
-            Summary = "teste fluent mapping",
-            Title = "Title"
-        };
-        context.Posts.Add(post);
-        context.SaveChanges();
     }
 }
